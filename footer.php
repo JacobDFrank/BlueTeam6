4c55dcb4ab5c6ff2fc994d8fbb229b4cf57766a7
Ben Alderfer <benalderfer974@gmail.com>
2016-11-06 18:45:47 -0500

    more styling and started basic files

---
+++ footer.php
@@ -0,0 +1,6 @@
+<script src="js/jquery-3.1.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<footer>
+
+</footer>
---
+++ head.php
@@ -0,0 +1,16 @@
+<meta charset='utf-8' />
+<meta name=viewport content='width=device-width, initial-scale=1'>
+<!-- Android Chrome Header Color -->
+<meta name="theme-color" content="#2196F3">
+
+<!-- Windows 8/10 resources -->
+<!--<meta name="msapplication-TileImage" content="images/favicon.png">-->
+<meta name="msapplication-TileColor" content="#2196F3">
+
+<!-- favicon -->
+<!--<link rel="shortcut icon" href="images/favicon.png" />-->
+
+<!-- style sheets -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css" media="screen">
+<link rel="stylesheet" href="css/print.css" media="print">

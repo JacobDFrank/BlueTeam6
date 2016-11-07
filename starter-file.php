69925789e7177f2d9d8338e275714e93a86c0622
Ben Alderfer <benalderfer974@gmail.com>
2016-11-06 19:50:21 -0500

    add starter file for other pages

---
+++ starter-file.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Title</title>
+    <?php include 'head.php' ?>
+</head>
+<body>
+<?php include 'header.php' ?>
+
+<section class="container">
+    <section class="row">
+
+    </section>
+</section>
+
+<?php include 'footer.php' ?>
+</body>
+</html>

c7ac83b847d28b33471adcd296254d9d744e7165
JacobDFrank <frankjacob42@gmail.com>
2016-11-09 20:55:48 -0500

    Added Header and Footer

--- header.php
+++ header.php
@@ -1,31 +1,41 @@
-<nav class="navbar navbar-inverse" id="navbar-custom">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-items" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-        </div>
+  <nav class="navbar navbar-fixed-top p-a-9 nav" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand title p-l-12 mobile-hide" href="index.php"><div class="tempHover">Blue Team 6</div></a>
+            </div>
+            <div id="menu" class="navbar-collapse collapse navbar-inner pull-right drop-down" >
+                <span class="x">
+                <button type="button" class="navbar-toggle exit" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <i class="close" aria-hidden="true"></i>
+                </button>
+                </span>
+                <ul class="nav navbar-nav navbar-right p-r-9 mobile-center menu-pad">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Courses <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="workshops.php">Workshops</a></li>
+                    <li class="text-nowrap"><a href="webinars.php">Webinars</a></li>
+                   <li><a href="faq.php">FAQ</a></li>
+                   <li><a class="nav-register menu-pad" href="contact.php">Contact</a></li>
+                </ul>
 
-        <!-- Links and dropdowns -->
-        <div class="collapse navbar-collapse" id="navbar-items">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
-                </li>
-            </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+    <div id="wrap-footer">
+        <div id="main-footer">
+            /////////header end, keep these two lines above/////////
+          

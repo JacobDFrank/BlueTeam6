258ee00443c8e491e4f07d5a3dd530b65f41e4b1
Ryan Sidebottom <daboss@Ryans-MacBook-Pro.local>
2016-11-10 01:32:37 -0500

    Created blogs.php,  incomplete, need to add blogs to it.

---
+++ blogs.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Blogs</title>
+		<?php include 'head.php' ?>
+	</head>
+	<body>
+		<?php include 'header.php' ?>
+
+		<section class="container">
+			<section class="row">
+				<!--SPLASH, image?, banner?-->
+				<img src="images/blogs/blog.jpg" alt="Blog">
+			</section>
+			<section class="row">
+				<div class="col-md-12">
+					<h1>Blog</h1>
+				</div>
+			</section>
+
+			<!--Security Blogs-->
+			<section id="security">
+                <!--BLOG POST 1-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>Expanded Security Tutorial</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                Blog numero uno.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+
+                <!--BLOG POST 2-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>Security Updates</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                Blog numero dos.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            </section>
+
+            <!--Android Blogs-->
+			<section id="android">
+                <!--BLOG POST 1-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>Android Blog 1</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                numero uno.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+
+                <!--BLOG POST 2-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>Android Blog 2</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                numero dos.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            </section>
+
+            <!--HTML/CSS Blogs-->
+			<section id="html/css">
+                <!--BLOG POST 1-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>HTML/CSS Blogs</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                numero uno
+                            </p>
+                        </div>
+                    </div>
+                </section>
+
+                <!--BLOG POST 2-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>HTML/CSS Blog 2</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                numero dos
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            </section>
+
+            <!--Database Blogs-->
+			<section id="database">
+                <!--BLOG POST 1-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>Database blog uno</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                numero uno
+                            </p>
+                        </div>
+                    </div>
+                </section>
+
+                <!--BLOG POST 2-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>Database blog 2</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                numero dos
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            </section>
+
+            <!--Java Blogs-->
+			<section id="java">
+                <!--BLOG POST 1-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>Java Blogss</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                numero uno
+                            </p>
+                        </div>
+                    </div>
+                </section>
+
+                <!--BLOG POST 2-->
+                <section class="row">
+                    <!--Blog Heading-->
+                    <div class="row">
+                        <h2>Java blogs 2</h2>
+                    </div>
+                    <!--Blog Post itself-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                numero dos
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            </section>
+		</section>
+
+		<?php include 'footer.php' ?>
+	</body>
+</html>

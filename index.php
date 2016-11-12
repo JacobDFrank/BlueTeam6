ab3e8704514bcd848cd33a264201f083c5389f5e
Ben Alderfer <benalderfer974@gmail.com>
2016-11-12 14:06:36 -0500

    started homepage

--- index.php
+++ index.php
@@ -8,8 +8,43 @@
 <?php include 'header.php' ?>
 
 <section class="container">
+    <!-- logo -->
     <section class="row">
+        <img src="images/logo.jpg" alt="Team logo">
+    </section>
 
+    <!-- title and subheader -->
+    <section class="row">
+        <div class="col-md-5">
+            <h2>Title and subheader</h2>
+        </div>
+        <div class="col-md-7"></div> <!-- spacer -->
+    </section>
+
+    <!-- about us -->
+    <section class="row">
+        <div class="col-md-4"></div> <!-- spacer -->
+        <div class="col-md-8 well">
+            <h3>About us</h3>
+            <p>about</p>
+        </div>
+    </section>
+
+    <!-- goals -->
+    <section class="row">
+        <div class="well">
+            <h3 class="centered">Goals</h3>
+            <p>goals</p>
+        </div>
+    </section>
+
+    <!-- newsletter -->
+    <section class="row">
+        <div class="col-md-10 well">
+            <h3>Newsletter</h3>
+            <p>newsletter</p>
+        </div>
+        <div class="col-md-2"></div> <!-- spacer -->
     </section>
 </section>
 

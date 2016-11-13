a2b7e9f40cfd444f12df47faa7f852f6242edca1
axd7832 <axd7832@g.rit.edu>
2016-11-13 18:22:36 -0500

    Webinar
    Worked on webinar
    Rough outline
    needs to be formatted better

--- quiz.php
+++ quiz.php
@@ -8,160 +8,168 @@
     <?php include 'header.php' ?>
 
 <div class="container">
-  <div class="well well-lg">
-    <div class="well well-sm">
-      <p>Question 1</p>
-      <form action= "quizsubmission.php" method="post" >
-        <div class="radio">
-          <label><input type="radio" name="optradio1" value = 1>Option 1</label>
+<div class= "row">
+  <div class="well">
+        <div class = "col-lg-6">
+          <h1> Persons Page </h1>
         </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio1" value = 2>Option 2</label>
+        <div class= "col-lg-6">
+          <img src="./images/andrew.jpg" class="img-responsive">
         </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio1" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio1" value = 4>Option 4</label>
-        </div>
-      </div>
       <div class="well">
-      <p>Question 2</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio2" value = 1>Option 1</label>
+        <p>Question 1</p>
+        <form action= "quizsubmission.php" method="post" >
+          <div class="radio">
+            <label><input type="radio" name="optradio1" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio1" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio1" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio1" value = 4>Option 4</label>
+          </div>
         </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio2" value = 2>Option 2</label>
+        <div class="well">
+        <p>Question 2</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio2" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio2" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio2" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio2" value = 4>Option 4</label>
+          </div>
         </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio2" value = 3>Option 3</label>
+        <div class="well">
+        <p>Question 3</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio3" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio3" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio3" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio3" value = 4>Option 4</label>
+          </div>
         </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio2" value = 4>Option 4</label>
+        <div class="well">
+        <p>Question 4</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio4" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio4" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio4" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio4" value = 4>Option 4</label>
+          </div>
         </div>
+        <div class="well">
+        <p>Question 5</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio5" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio5" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio5" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio5" value = 4>Option 4</label>
+          </div>
+        </div>
+        <div class="well">
+        <p>Question 6</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio6" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio6" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio6" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio6" value = 4>Option 4</label>
+          </div>
+        </div>
+        <div class="well">
+        <p>Question 7</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio7" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio7" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio7" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio7" value = 4>Option 4</label>
+          </div>
+        </div>
+        <div class="well">
+        <p>Question 8</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio8" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio8" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio8" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio8" value = 4>Option 4</label>
+          </div>
+        </div>
+        <div class="well">
+        <p>Question 9</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio9" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio9" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio9" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio9" value = 4>Option 4</label>
+          </div>
+        </div>
+        <div class="well">
+        <p>Question 10</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio10" value = 1>Option 1</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio10" value = 2>Option 2</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio10" value = 3>Option 3</label>
+          </div>
+          <div class="radio">
+            <label><input type="radio" name="optradio10" value = 4>Option 4</label>
+          </div>
+        </div>
+          <input type="submit" class="btn btn-lg btn-primary" name="submit" value="Submit"/>
+        </form>
       </div>
-      <div class="well">
-      <p>Question 3</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio3" value = 1>Option 1</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio3" value = 2>Option 2</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio3" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio3" value = 4>Option 4</label>
-        </div>
-      </div>
-      <div class="well">
-      <p>Question 4</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio4" value = 1>Option 1</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio4" value = 2>Option 2</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio4" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio4" value = 4>Option 4</label>
-        </div>
-      </div>
-      <div class="well">
-      <p>Question 5</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio5" value = 1>Option 1</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio5" value = 2>Option 2</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio5" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio5" value = 4>Option 4</label>
-        </div>
-      </div>
-      <div class="well">
-      <p>Question 6</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio6" value = 1>Option 1</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio6" value = 2>Option 2</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio6" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio6" value = 4>Option 4</label>
-        </div>
-      </div>
-      <div class="well">
-      <p>Question 7</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio7" value = 1>Option 1</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio7" value = 2>Option 2</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio7" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio7" value = 4>Option 4</label>
-        </div>
-      </div>
-      <div class="well">
-      <p>Question 8</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio8" value = 1>Option 1</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio8" value = 2>Option 2</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio8" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio8" value = 4>Option 4</label>
-        </div>
-      </div>
-      <div class="well">
-      <p>Question 9</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio9" value = 1>Option 1</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio9" value = 2>Option 2</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio9" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio9" value = 4>Option 4</label>
-        </div>
-      </div>
-      <div class="well">
-      <p>Question 10</p>
-        <div class="radio">
-          <label><input type="radio" name="optradio10" value = 1>Option 1</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio10" value = 2>Option 2</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio10" value = 3>Option 3</label>
-        </div>
-        <div class="radio">
-          <label><input type="radio" name="optradio10" value = 4>Option 4</label>
-        </div>
-      </div>
-        <input type="submit" class="btn btn-lg btn-primary" name="submit" value="Submit"/>
-      </form>
     </div>
 </div>
 <?php include 'footer.php' ?>
---
+++ webinars.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Webinars</title>
+    <?php include 'head.php' ?>
+</head>
+<body>
+    <?php include 'header.php' ?>
+
+<div class="container">
+  <div class= "row">
+    <h1> WEBINARS </h1>
+    <div class="well col-lg-5 col-sm-12 webinars">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+  <div class="well col-lg-5 col-sm-12 webinars ">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+  <div class="well col-lg-5 col-sm-12 webinars">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+  <div class="well col-lg-5 col-sm-12 webinars ">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+
+<div class="well col-lg-5 col-sm-12 webinars">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+  <div class="well col-lg-5 col-sm-12 webinars ">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+
+<div class="well col-lg-5 col-sm-12 webinars">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+  <div class="well col-lg-5 col-sm-12 webinars ">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+
+<div class="well col-lg-5 col-sm-12 webinars">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+  <div class="well col-lg-5 col-sm-12 webinars ">
+      <div class="text-center">
+      <h2>WEBINAR</h2>
+      </div>
+      Title: INSERT TITLE HERE<br>
+      Date: DATE HERE<br>
+      INSTRUCTOR: PERSON 1<br>
+      DESCRIPTION: <p> LOREM IPSUM </p
+    </div>
+  </div>
+
+
+
+</div>
+<?php include 'footer.php' ?>
+</body>
+</html>

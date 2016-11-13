f9836126bd29f847ab43ae81738bac09f9732aa3
Ben Alderfer <benalderfer974@gmail.com>
2016-11-13 16:38:50 -0500

    work on android tutorial

--- tutorial-android.php
+++ tutorial-android.php
@@ -32,31 +32,83 @@
         </section>
 
         <section class="well">
-            <!-- content left, picture right -->
+            <!-- full content -->
             <section class="row tutorial_content-row well">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <h2>Step 1</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus enim metus, at ultricies risus facilisis sit amet. Suspendisse nec justo massa. Pellentesque eleifend rhoncus est, quis auctor augue ultrices at. Nulla ut ex sed augue tempus tempus sodales nec mi. Praesent rhoncus, elit sed consequat euismod, dolor lacus imperdiet metus, sed gravida lacus quam at neque. Cras fermentum, nunc ac maximus laoreet, enim nisi sollicitudin felis, eget volutpat orci odio eget libero. Duis nec euismod massa, vel sollicitudin ante.</p>
-                </div>
-                <img src="images/tutorials/android/empty_layout.png" alt="Step picture" class="col-md-6">
-            </section>
-
-            <!-- picture left, content right -->
-            <section class="row tutorial_content-row well">
-                <img src="images/tutorials/android/base_layout.png" alt="Step picture" class="col-md-6">
-                <div class="col-md-6">
-                    <h2>Step 2</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus enim metus, at ultricies risus facilisis sit amet. Suspendisse nec justo massa. Pellentesque eleifend rhoncus est, quis auctor augue ultrices at. Nulla ut ex sed augue tempus tempus sodales nec mi. Praesent rhoncus, elit sed consequat euismod, dolor lacus imperdiet metus, sed gravida lacus quam at neque. Cras fermentum, nunc ac maximus laoreet, enim nisi sollicitudin felis, eget volutpat orci odio eget libero. Duis nec euismod massa, vel sollicitudin ante.</p>
+                    <p>Before starting, you will need to download <a href="https://developer.android.com/studio/index.html">Android Studio</a> and the Android SDK (during the Android Studio installation).</p>
                 </div>
             </section>
 
             <!-- full content -->
             <section class="row tutorial_content-row well">
-                <div>
-                    <h2>Step 3</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus enim metus, at ultricies risus facilisis sit amet. Suspendisse nec justo massa. Pellentesque eleifend rhoncus est, quis auctor augue ultrices at. Nulla ut ex sed augue tempus tempus sodales nec mi. Praesent rhoncus, elit sed consequat euismod, dolor lacus imperdiet metus, sed gravida lacus quam at neque. Cras fermentum, nunc ac maximus laoreet, enim nisi sollicitudin felis, eget volutpat orci odio eget libero. Duis nec euismod massa, vel sollicitudin ante.</p>
+                <div class="col-md-12">
+                    <h2>Step 2</h2>
+                    <p>Create a new app. Name it whatever you want and set your domain to your website or make something up. Press “Next”. Set the minimum SDK level to 11 and press “Next”. Select “Empty Activity” and press “Next” and then press “Finish”. You are now ready to start making the app.</p>
                 </div>
             </section>
+
+            <!-- full content -->
+            <section class="row tutorial_content-row well">
+                <div class="col-md-12">
+                    <h2>Step 3</h2>
+                    <p>First, we are going to define colors. Go to “app>src>main>res>values>colors.xml”. If the file does not exist, create it.</p>
+                </div>
+            </section>
+
+            <!-- full content -->
+            <section class="row tutorial_content-row well">
+                <div class="col-md-12">
+                    <h2>Step 4</h2>
+                    <p>In that file, set the colorPrimary, colorPrimaryDark, and colorAccent. The file should look like this: <br />
+                        <code>
+                            &lt;?xml version="1.0" encoding="utf-8"?&gt; <br />
+                            &lt;resources&gt; <br />
+                                &lt;color name="colorPrimary">#2196F3&lt;/color&gt; <br />
+                                &lt;color name="colorPrimaryDark">#1976D2&lt;/color&gt; <br />
+                                &lt;color name="colorAccent">#FF9800&lt;/color&gt; <br />
+                                &lt;color name="white">#FFFFFF&lt;/color&gt; <br />
+                            &lt;/resources&gt;
+                        </code>
+                    </p>
+                </div>
+            </section>
+
+            <!-- full content -->
+            <section class="row tutorial_content-row well">
+                <div class="col-md-12">
+                    <h2>Step 5</h2>
+                    <p>Now we will set up the theme. Go to “app>src>main>res>values>styles.xml”. If the file does not exist, create it. In the file, you will need to customize the styles to match your design and add the “Toolbar” style. You can either make your own or use this: <br />
+                        <code>
+                            &lt;resources&gt; <br />
+                                <!-- Base application theme. -->
+                                &lt;style name="AppTheme" parent="Theme.AppCompat.Light.NoActionBar"&gt; <br />
+                                    &lt;item name="colorPrimary"&gt;@color/colorPrimary&lt;/item&gt; <br />
+                                    &lt;item name="colorPrimaryDark"&gt;@color/colorPrimaryDark&lt;/item&gt; <br />
+                                    &lt;item name="colorAccent"&gt;@color/colorAccent&lt;/item&gt; <br />
+                                &lt;/style&gt; <br /> <br />
+
+                                <!-- Toolbar theme. -->
+                                &lt;style name="Toolbar" parent="Theme.AppCompat.Light.NoActionBar"&gt; <br />
+                                    &lt;item name="android:windowNoTitle"&gt;true&lt;/item&gt; <br />
+                                    &lt;item name="android:windowActionBar"&gt;false&lt;/item&gt; <br />
+                                    &lt;item name="android:textColorPrimary"&gt;@color/white&lt;/item&gt; <br />        <!-- activity name text color -->
+                                    &lt;item name="android:background"&gt;@color/colorPrimary&lt;/item&gt; <br />        <!-- bar background color -->
+                                &lt;/style&gt; <br />
+                            &lt;/resources&gt;
+                        </code>
+                    </p>
+                </div>
+            </section>
+
+            <!-- full content -->
+            <section class="row tutorial_content-row well">
+                <div class="col-md-12">
+                    <h2>Step 6</h2>
+                    <p>First, we are going to define colors. Go to “app>src>main>res>values>colors.xml”. If the file does not exist, create it.</p>
+                </div>
+            </section>
+
         </section>
     </section>
     <?php include 'footer.php' ?>

c41dc964aafd80a668c87fe9c10f8e90bd5ea424
Ryan Sidebottom <daboss@daboss.wireless.rit.edu>
2016-11-15 13:56:24 -0500

    Added personal page template.

---
+++ personal.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Topic Goes Here</title>
+    <?php include 'head.php' ?>
+</head>
+<body>
+    <?php include 'header.php' ?>
+
+    <section class="container">
+        <section class="row" id="personHead">
+            <div class="col-lg-4">
+                <h1>Person Page/Topic</h1>
+            </div>
+            <div class="col-lg-4 pull-right">
+                <img src="images/ryan.png"/>
+            </div>
+        </section>
+        <section class="row top-buffer" id="topic">
+            <div class="col-md-6">
+                <p>THIS IS THE CONTENT BITCHES.</p>
+            </div>
+            <div class="col-md-4 pull-right">
+                <div class="row">
+                    <a href="" class="btn btn-default">Tutorial</a>
+                </div>
+                <div class="row top-buffer">
+                    <a href="" class="btn btn-default">Quiz</a>
+                </div>
+            </div>
+        </section>
+        <section class="row" id="testimonials">
+            <h2>Testimonials</h2>
+            <div class="col-md-5">
+                <p>Testimonial uno.</p>
+            </div>
+            <div class="col-md-5 pull-right">
+                <p>Testimonial dos.</p>
+            </div>
+        </section>
+        <section class="row" id="strengths">
+            <h2>My Strengths and Abilities</h2>
+            <p>What am I good at?</p>
+        </section>
+        <section class="row" id="aboutMe">
+            <h2>Whoami?</h2>
+            <p>I am a person.</p>
+        </section>
+    </section>
+
+    <?php include 'footer.php' ?>
+</body>
+</html>

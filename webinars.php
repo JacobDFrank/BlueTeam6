23b923e2c8cee52b4f60b6b01edde95dc97c465e
Ben Alderfer <benalderfer974@gmail.com>
2016-11-15 19:32:07 -0500

    fixed webinar page

--- webinars.php
+++ webinars.php
@@ -7,116 +7,112 @@
 <body>
     <?php include 'header.php' ?>
 
-<div class="container">
-  <div class= "row">
-    <h1> WEBINARS </h1>
-    <div class="well col-lg-5 col-sm-12 webinars">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
-  <div class="well col-lg-5 col-sm-12 webinars ">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
-  <div class="well col-lg-5 col-sm-12 webinars">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
-  <div class="well col-lg-5 col-sm-12 webinars ">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
+    <div class="container">
+      <div class= "row">
+        <h1> WEBINARS </h1>
 
-<div class="well col-lg-5 col-sm-12 webinars">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
-  <div class="well col-lg-5 col-sm-12 webinars ">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+            <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
 
-<div class="well col-lg-5 col-sm-12 webinars">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
-  <div class="well col-lg-5 col-sm-12 webinars ">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
 
-<div class="well col-lg-5 col-sm-12 webinars">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
+
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
+
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
+
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
+
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
+
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
+
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
+
+        <div class="well col-lg-5 col-sm-12 webinars">
+          <div class="text-center">
+              <h2>WEBINAR</h2>
+          </div>
+          Title: INSERT TITLE HERE<br/>
+          Date: DATE HERE<br/>
+          INSTRUCTOR: PERSON 1<br/>
+          DESCRIPTION: <p> LOREM IPSUM </p>
+        </div>
       </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
     </div>
-  </div>
-  <div class="well col-lg-5 col-sm-12 webinars ">
-      <div class="text-center">
-      <h2>WEBINAR</h2>
-      </div>
-      Title: INSERT TITLE HERE<br>
-      Date: DATE HERE<br>
-      INSTRUCTOR: PERSON 1<br>
-      DESCRIPTION: <p> LOREM IPSUM </p
-    </div>
-  </div>
 
-
-
-</div>
-<?php include 'footer.php' ?>
+    <?php include 'footer.php' ?>
 </body>
 </html>

2dfe09c200f23b5143e9278a5bad2e208f666dbf
Jesus <je6095@rit.edu>
2016-11-17 12:08:37 -0500

    sitemap and grading

---
+++ sitemap.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>FAQ</title>
+    <?php include 'head.php' ?>
+</head>
+	<body>
+	<?php include 'header.php' ?>
+		<h1>Sitemap</h1>
+		<section class="container">
+		
+		</section>
+	<?php include 'footer.php' ?>	
+	</body>
+</html>	

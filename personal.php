b41f7c0eb71b1cf6ba1710ffa7376398afca6e3a
Ryan Sidebottom <daboss@daboss.wireless.rit.edu>
2016-11-19 14:37:52 -0500

    Fixed blog/personal formatting with images

--- personal.php
+++ personal.php
@@ -9,27 +9,28 @@
 
     <section class="container">
         <section class="row" id="personHead">
-            <div class="col-lg-4">
+            <div class="col-md-7">
                 <h1>Person Page/Topic</h1>
             </div>
-            <div class="col-lg-4 pull-right">
-                <img src="images/ryan.png"/>
-            </div>
+            <div class="col-md-2"></div>
+<!--            <div class="col-md-3 col-sm-6">-->
+                <img class="col-md-3 col-sm-6" src="images/ryan.png"/>
+<!--            </div>-->
         </section>
         <section class="row top-buffer" id="topic">
-            <div class="col-md-6">
-                <p>THIS IS THE CONTENT BITCHES.</p>
+            <div class="col-md-8">
+                <p>This is how you topic. Here is what topic is. I am good at topic.</p>
             </div>
             <div class="col-md-4 pull-right">
                 <div class="row">
-                    <a href="" class="btn btn-default">Tutorial</a>
+                    <a href="" class="btn btn-primary col-sm-12">Tutorial</a>
                 </div>
                 <div class="row top-buffer">
-                    <a href="" class="btn btn-default">Quiz</a>
+                    <a href="" class="btn btn-primary col-sm-12">Quiz</a>
                 </div>
             </div>
         </section>
-        <section class="row" id="testimonials">
+        <section class="row well top-buffer" id="testimonials">
             <h2>Testimonials</h2>
             <div class="col-md-5">
                 <p>Testimonial uno.</p>
@@ -38,11 +39,11 @@
                 <p>Testimonial dos.</p>
             </div>
         </section>
-        <section class="row" id="strengths">
+        <section class="row well" id="strengths">
             <h2>My Strengths and Abilities</h2>
             <p>What am I good at?</p>
         </section>
-        <section class="row" id="aboutMe">
+        <section class="row well" id="aboutMe">
             <h2>Whoami?</h2>
             <p>I am a person.</p>
         </section>

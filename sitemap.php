188f4ca9d1a8b65f99ff665478a3861275e55275
Jesus <je6095@rit.edu>
2016-11-21 21:16:11 -0500

    faq and sitemap edits

--- sitemap.php
+++ sitemap.php
@@ -8,7 +8,26 @@
 	<?php include 'header.php' ?>
 		<h1>Sitemap</h1>
 		<section class="container">
-		
+		<a href="index.php">Home</a>
+		<br>
+		Courses
+		<br>
+		<ul style="margin-left=10px;">
+			   <li><a href="#">Java</a></li>
+			   <li><a href="#">HTML/CSS</a></li>
+			   <li><a href="#">Android</a></li>
+			   <li><a href="#">Security</a></li>
+			   <li><a href="#">Database</a></li>
+		</ul>
+		<a href="workshops.php">Workshops</a>
+		<br>
+		<a href="webinars.php">Webinars</a>
+		<br>
+		<a href="faq.php">FAQ</a>
+		<br>
+		<a href="contact.php">Contact Us</a>
+		<br>
+		<a href="grading.php">Grading</a>
 		</section>
 	<?php include 'footer.php' ?>	
 	</body>

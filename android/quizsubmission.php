da2bbb29e7500731479d8d2e44a90b2c6c674f6e
Ben Alderfer <benalderfer974@gmail.com>
2016-11-24 23:10:23 -0500

    finished quiz and added inner header

--- android/quizsubmission.php
+++ android/quizsubmission.php
@@ -21,215 +21,125 @@
 	$q10sub = $_POST['optradio10'];
 
 	//Change correct $answer#= X to the correct answer and move case statements accordingly 
-	$answer1 = 1;
+	$answer1 = 2;
 	echo "<div class = \"container\"><div class= \"well\">";
 	echo "Question 1: ";
 	switch($q1sub)
 		{
-				case 1:
+				case $answer1:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer1 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer1 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer1 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer1 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer1 ."<br>";
 					break;
 		}
-	$answer2 = 1;
+	$answer2 = 3;
 	echo "Question 2: ";
 	switch($q2sub)
 		{
-				case 1:
+				case $answer2:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer2 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer2 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer2 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer2 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer2 ."<br>";
 					break;
 		}
 	$answer3 = 1;
 	echo "Question 3: ";
 	switch($q3sub)
 		{
-				case 1:
+				case $answer3:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer3 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer3 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer3 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer3 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer3 ."<br>";
 					break;
 		}
-	$answer4 = 1;
+	$answer4 = 4;
 	echo "Question 4: ";
 	switch($q4sub)
 		{
-				case 1:
+				case $answer4:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer1 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer1 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer1 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer1 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer1 ."<br>";
 					break;
 		}
-	$answer5 = 1;
+	$answer5 = 4;
 	echo "Question 5: ";
 	switch($q5sub)
 		{
-				case 1:
+				case $answer5:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer5 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer5 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer5 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer5 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer5 ."<br>";
 					break;
 		}
 	$answer6 = 1;
 	echo "Question 6: ";
 	switch($q6sub)
 		{
-				case 1:
+				case $answer6:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer6 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer6 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer6 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer6 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer6 ."<br>";
 					break;
 		}
-	$answer7 = 1;
+	$answer7 = 3;
 	echo "Question 7: ";
 	switch($q7sub)
 		{
-				case 1:
+				case $answer7:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer7 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer7 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer7 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer7 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer7 ."<br>";
 					break;
 		}
-	$answer8 = 1;
+	$answer8 = 4;
 	echo "Question 8: ";
 	switch($q8sub)
 		{
-				case 1:
+				case $answer8:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer8 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer8 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer8 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer8 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer8 ."<br>";
 					break;
 		}
-	$answer9 = 1;
-	echo "Question 2: ";
+	$answer9 = 2;
+	echo "Question 9: ";
 	switch($q2sub)
 		{
-				case 1:
+				case $answer9:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer9. "<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer9 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer9 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer9 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer9 ."<br>";
 					break;
 		}
-	$answer10 = 1;
+	$answer10 = 3;
 	echo "Question 10: ";
 	switch($q10sub)
 		{
-				case 1:
+				case $answer10:
 					echo "Correct. Good Job! <br>";
 					$totalCorrect += 1;
 					break;
-				case 2:
-					echo "Oops! The Correct answer was question : ". $answer10 ."<br>";
-					break;
-				case 3:
-					echo "Oops! The Correct answer was question : ". $answer10 ."<br>";
-					break;
-				case 4:
-					echo "Oops! The Correct answer was question : ". $answer10 ."<br>";
-					break;
 				default:
-					echo "Oops! The Correct answer was question : ". $answer10 ."<br>";
+					echo "Oops! The correct answer was question : ". $answer10 ."<br>";
 					break;
 		}
 	echo "You got ". $totalCorrect . "/10 correct.</div></div>";
---
+++ inner-header.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-fixed-top p-a-9 nav" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand title p-l-12 mobile-hide" href="../index.php"><div class="tempHover">Blue Team 6</div></a>
+        </div>
+        <div id="menu" class="navbar-collapse collapse navbar-inner pull-right drop-down" >
+            <span class="x">
+            <button type="button" class="navbar-toggle exit" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <i class="close" aria-hidden="true"></i>
+            </button>
+            </span>
+            <ul class="nav navbar-nav navbar-right p-r-9 mobile-center menu-pad">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Courses <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="../#">Java</a></li>
+                        <li><a href="../#">HTML/CSS</a></li>
+                        <li><a href="../#">Android</a></li>
+                        <li><a href="../#">Computer Security</a></li>
+                        <li><a href="../#">Database Programming</a></li>
+                    </ul>
+                </li>
+                <li><a href="../blogs.php">Blog</a></li>
+                <li><a href="../workshops.php">Workshops</a></li>
+                <li class="text-nowrap"><a href="../webinars.php">Webinars</a></li>
+                <li><a href="../faq.php">FAQ</a></li>
+                <li><a class="nav-register menu-pad" href="../contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div id="header-spacer">&nbsp;</div>
+<script src="js/nav.js"></script>
+            <!-- header end, keep these two lines above -->

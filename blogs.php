5ad717d95dfcca276bb18d82d578150b4e168985
Ryan Sidebottom <daboss@DaBoss.hsd1.nj.comcast.net>
2016-11-25 14:59:35 -0500

    Completing blogs page

--- blogs.php
+++ blogs.php
@@ -2,17 +2,17 @@
 <html lang="en">
 	<head>
 		<title>Blogs</title>
-		<?php include '../inner-head.php' ?>
+		<?php include 'head.php' ?>
 	</head>
 	<body>
-		<?php include '../header.php' ?>
+		<?php include 'header.php' ?>
 
 		<section class="container">
 			<section class="row">
                 <div class="col-md-4"></div>
 				<!--SPLASH, image?, banner?-->
 				<div class="col-md-4">
-                    <img src="../images/blogs/blog.png" alt="Blog">
+                    <img src="images/blogs/blog.png" alt="Blog">
                 </div>
                 <div class="col-md-4"></div>
 			</section>
@@ -23,7 +23,9 @@
                         <h1>Blog</h1>
                 </div>
             </section>
-			<section class="well top-buffer">
+
+			<section class="well top-buffer" id="security">
+                <h2>Security</h2>
                 <!--BLOG POST 1-->
                 <section class="row top-buffer">
                     <div class="col-md-1"></div>
@@ -32,7 +34,69 @@
                         <div class="row">
                             <div class="col-md-2"></div>
                             <div class="col-md-8">
-                                <h2>Blog Title</h2>
+                                <h3>Security Tutorial Expnsion!</h3>
+                            </div>
+                            <div class="col-md-2"></div>
+                        </div>
+                        <!--Blog Post itself-->
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-10">
+                                <p>
+                                    Hey all, <br />
+                                    After the positive feedback on my Introduction to Network Security, I have decided to expand my tutorial and security program overall! Within the next month you can expect to see a new section that will discuss application security! Pending on the feedback from that, I might dive into a subsection of application security, secure coding! With this new section being added to my portion of the site, I will also add quiz questions that reflect the updated changes. In addition to this, I am working on setting up a couple of workshops where I can teach some security techniques and practices live to you all! Updates on this will come around soon but at the moment I’m working out the feasibility of such a feat! I look forward to dropping more content for you all!<br />
+                                    Cheers!<br />
+                                    --Ryan
+                                </p>
+                            </div>
+                            <div class="col-md-1"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                </section>
+
+                <!--BLOG POST 2-->
+                <section class="row top-buffer">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10 well">
+                        <!--Blog Heading-->
+                        <div class="row">
+                            <div class="col-md-2"></div>
+                            <div class="col-md-8">
+                                <h3>Moar Security!</h3>
+                            </div>
+                            <div class="col-md-2"></div>
+                        </div>
+                        <!--Blog Post itself-->
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-10">
+                                <p>
+                                    Hey all,<br />
+                                    Welcome back to my blog everyone! I have quite a number of updates for you all this time, so let’s get to them! As you all have noticed by now, I have updated the site to include the Introduction to Application Security! Inside of this topic, I have also began to include information about secure coding! Currently, only the section on Front End coding is up however, I’m working on adding a section about Back End coding to complete that topic! In addition to that, I will be adding material to the quiz as well to cover all section of the site. Last month I mentioned that I was working on setting up a couple of online workshops well, I can say that I successfully set up two workshops! These workshops will not start until after the holiday season and the years turns over. Until then, think of a topic you would like me to cover! I have a few in mind however, if there is a lot of desire for different topics I will send out a poll in the newsletter and then go with the most popular topic! Don’t fret if you can’t make the workshop as I will create a tutorial on the website after the workshop is complete! If there is a certain topic you would like me to cover (in the workshop or just on the website), feel free to email it to me at <a href='mailto&#58;r%7&#57;ano&#110;%73%&#54;&#53;c%7&#53;ri%7&#52;&#121;&#64;&#37;&#54;&#50;&#108;u&#101;&#116;e%61&#109;six&#46;&#99;om'>ry&#97;non&#115;ecu&#114;i&#116;&#121;&#64;blu&#101;&#116;eamsix&#46;com</a>! That’s all for today everyone!<br />
+                                    Cheers!<br />
+                                    --Ryan
+                                </p>
+                            </div>
+                            <div class="col-md-1"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                </section>
+            </section>
+
+<!--            BLOG TEMPLATE-->
+            <section class="well top-buffer" id="subject">
+                <h2>Subject</h2>
+                <!--BLOG POST 1-->
+                <section class="row top-buffer">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10 well">
+                        <!--Blog Heading-->
+                        <div class="row">
+                            <div class="col-md-2"></div>
+                            <div class="col-md-8">
+                                <h3>Blog Title</h3>
                             </div>
                             <div class="col-md-2"></div>
                         </div>
@@ -58,7 +122,7 @@
                         <div class="row">
                             <div class="col-md-2"></div>
                             <div class="col-md-8">
-                                <h2>Blog Title</h2>
+                                <h3>Blog Title</h3>
                             </div>
                             <div class="col-md-2"></div>
                         </div>
@@ -78,6 +142,6 @@
             </section>
 		</section>
 
-		<?php include '../footer.php' ?>
+		<?php include 'footer.php' ?>
 	</body>
 </html>

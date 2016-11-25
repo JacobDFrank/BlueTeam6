e0f3c2c91ffe6bb738d6c3ba578128722088e043
Ben Alderfer <benalderfer974@gmail.com>
2016-11-25 18:18:20 -0500

    filled in webinar

--- webinars.php
+++ webinars.php
@@ -9,26 +9,26 @@
 
     <div class="container">
       <div class= "row">
-        <h1> WEBINARS </h1>
+        <h1>WEBINARS</h1>
 
         <div class="well col-lg-5 col-sm-12">
           <div class="text-center">
             <h2>WEBINAR</h2>
           </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+          Title: Vector Drawables<br/>
+          Date: December 29, 2016<br/>
+          Instructor: Ben Alderfer<br/>
+          Description: <p> In this webinar we’ll discuss the benefits of vector drawables. We will cover how to create them, the structure for naming and storing them, and how to incorporate them in apps. We will also discuss where you can find some pre-made drawables to avoid having to recreate them yourself. </p>
         </div>
         <div class= "col-lg-2"></div><!--Spacer -->
         <div class="well col-lg-5 col-sm-12"> 
           <div class="text-center">
               <h2>WEBINAR</h2>
           </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+          Title: Material Design<br/>
+          Date: January 17, 2017<br/>
+            Instructor: Ben Alderfer<br/>
+            Description: <p> In this webinar we will discuss Google’s design philosophy, Material Design. We will discuss basics such as cards, buttons, and animations. We will explore not only the specific design instructions, but also the way of thinking that drives Material Design. We will use various resources to create a beautiful app that meets the standards of Material Design. </p>
         </div>
         <div class="well col-lg-5 col-sm-12">
           <div class="text-center">
@@ -36,8 +36,8 @@
           </div>
           Title: INSERT TITLE HERE<br/>
           Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+            Instructor: PERSON 1<br/>
+            Description: <p> LOREM IPSUM </p>
         </div>
         <div class= "col-lg-2"></div><!--Spacer --> 
         <div class="well col-lg-5 col-sm-12">
@@ -46,8 +46,8 @@
           </div>
           Title: INSERT TITLE HERE<br/>
           Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+            Instructor: PERSON 1<br/>
+            Description: <p> LOREM IPSUM </p>
         </div>
 
         <div class="well col-lg-5 col-sm-12">
@@ -56,8 +56,8 @@
           </div>
           Title: INSERT TITLE HERE<br/>
           Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+            Instructor: PERSON 1<br/>
+            Description: <p> LOREM IPSUM </p>
         </div>
         <div class= "col-lg-2"></div><!--Spacer -->
         <div class="well col-lg-5 col-sm-12">
@@ -66,8 +66,8 @@
           </div>
           Title: INSERT TITLE HERE<br/>
           Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+            Instructor: PERSON 1<br/>
+            Description: <p> LOREM IPSUM </p>
         </div>
 
         <div class="well col-lg-5 col-sm-12">
@@ -76,8 +76,8 @@
           </div>
           Title: INSERT TITLE HERE<br/>
           Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+            Instructor: PERSON 1<br/>
+            Description: <p> LOREM IPSUM </p>
         </div>
         <div class= "col-lg-2"></div><!--Spacer -->
         <div class="well col-lg-5 col-sm-12">
@@ -86,8 +86,8 @@
           </div>
           Title: INSERT TITLE HERE<br/>
           Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+            Instructor: PERSON 1<br/>
+            Description: <p> LOREM IPSUM </p>
         </div>
 
         <div class="well col-lg-5 col-sm-12 ">
@@ -96,8 +96,8 @@
           </div>
           Title: INSERT TITLE HERE<br/>
           Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+            Instructor: PERSON 1<br/>
+            Description: <p> LOREM IPSUM </p>
         </div>
         <div class= "col-lg-2"></div><!--Spacer -->
         <div class="well col-lg-5 col-sm-12">
@@ -106,8 +106,8 @@
           </div>
           Title: INSERT TITLE HERE<br/>
           Date: DATE HERE<br/>
-          INSTRUCTOR: PERSON 1<br/>
-          DESCRIPTION: <p> LOREM IPSUM </p>
+            Instructor: PERSON 1<br/>
+            Description: <p> LOREM IPSUM </p>
         </div>
       </div>
     </div>

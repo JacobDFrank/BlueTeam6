5a3bbbab18b23610c1fc65d486b69e62e954f079
Ryan Sidebottom <daboss@DaBoss.hsd1.nj.comcast.net>
2016-11-26 01:44:21 -0500

    Added rest of webinars, adjusted layout

--- webinars.php
+++ webinars.php
@@ -8,108 +8,117 @@
     <?php include 'header.php' ?>
 
     <div class="container">
-      <div class= "row">
         <h1>WEBINARS</h1>
 
-        <div class="well col-lg-5 col-sm-12">
-          <div class="text-center">
-            <h2>Android Webinar I</h2>
-          </div>
-          Title: Vector Drawables<br/>
-          Date: December 29, 2016<br/>
-          Instructor: Ben Alderfer<br/>
-          Description: <p> In this webinar we’ll discuss the benefits of vector drawables. We will cover how to create them, the structure for naming and storing them, and how to incorporate them in apps. We will also discuss where you can find some pre-made drawables to avoid having to recreate them yourself. </p>
-        </div>
-        <div class= "col-lg-2"></div><!--Spacer -->
-        <div class="well col-lg-5 col-sm-12"> 
-          <div class="text-center">
-              <h2>Android Webinar II</h2>
-          </div>
-          Title: Material Design<br/>
-          Date: January 17, 2017<br/>
-            Instructor: Ben Alderfer<br/>
-            Description: <p> In this webinar we will discuss Google’s design philosophy, Material Design. We will discuss basics such as cards, buttons, and animations. We will explore not only the specific design instructions, but also the way of thinking that drives Material Design. We will use various resources to create a beautiful app that meets the standards of Material Design. </p>
-        </div>
-        <div class="well col-lg-5 col-sm-12">
-          <div class="text-center">
-              <h2>Security Webinar I</h2>
-          </div>
-          Title: Checking the Security of YOUR Servers<br/>
-          Date: December 21, 2016<br/>
-            Instructor: Ryan Sidebottom<br/>
-            Description: <p> In this workshop I will give you an introduction to securing your servers by showing you how to examine how secure they actually are. Securing a server is relatively simple once you know what is wrong, or insecure about it. Once you know what is insecure, you can work at making that insecurity more secure. Sometimes it is not possible to close off as many ports as possible but it is always possible to manage connections. We will go over some tools you can use to evaluate your server and then go over some remediation methods. <strong>Disclaimer: some or all of the tools we will discuss in this workshop can be used in a malicious manner. Never do anything you do not have permission to do. In the workshop we will be scanning our own server, so it is best that you also use your own server. You cannot use the tools against our server or any other server in which you do not have written permission to evaluate.</strong> </p>
-        </div>
-        <div class= "col-lg-2"></div><!--Spacer --> 
-        <div class="well col-lg-5 col-sm-12">
-          <div class="text-center">
-              <h2>Security Webinar II</h2>
-          </div>
-          Title: Bug Bounty and Ethical Hacking<br/>
-          Date: January 21, 2017<br/>
-            Instructor: Ryan Sidebottom<br/>
-            Description: <p> In this workshop we will go over what a Bug Bounty is and what Ethical Hacking is. Using some of the tools and techniques we teach you in the previous workshop, you can get permission from certain website and companies to try and “break” their website. Basically, you can do some Ethical Hacking and get paid. Most of the companies that offer a Bug Bounty program will be pretty secure so if you are truly interested in being successful and finding a weak link in a website, you will need to learn and research more than what I go over in this presentation. We will also do a brief recap of some of the topics covered in the last workshop since they are relevant in this one. <strong>Disclaimer: Some or all of the tools we will discuss in this workshop can be used in a malicious manner. We will be demonstrating on one of our personal servers again which are off limits to you. Remember, you cannot use the tools against our server or any other server in which you do not have written permission to attack or scan.</strong> </p>
-        </div>
+        <section class="row">
+            <div class="well col-lg-5 col-sm-12">
+            <div class="text-center">
+                <h2>Android Webinar I</h2>
+            </div>
+                Title: Vector Drawables<br/>
+                Date: December 29, 2016<br/>
+                Instructor: Ben Alderfer<br/>
+                Description: <p> In this webinar we’ll discuss the benefits of vector drawables. We will cover how to create them, the structure for naming and storing them, and how to incorporate them in apps. We will also discuss where you can find some pre-made drawables to avoid having to recreate them yourself. </p>
+            </div>
+            <div class= "col-lg-2"></div><!--Spacer -->
+            <div class="well col-lg-5 col-sm-12">
+                <div class="text-center">
+                  <h2>Android Webinar II</h2>
+                </div>
+                Title: Material Design<br/>
+                Date: January 17, 2017<br/>
+                Instructor: Ben Alderfer<br/>
+                Description: <p> In this webinar we will discuss Google’s design philosophy, Material Design. We will discuss basics such as cards, buttons, and animations. We will explore not only the specific design instructions, but also the way of thinking that drives Material Design. We will use various resources to create a beautiful app that meets the standards of Material Design. </p>
+            </div>
+        </section>
 
-        <div class="well col-lg-5 col-sm-12">
-          <div class="text-center">
-              <h2>WEBINAR</h2>
-          </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-            Instructor: PERSON 1<br/>
-            Description: <p> LOREM IPSUM </p>
-        </div>
-        <div class= "col-lg-2"></div><!--Spacer -->
-        <div class="well col-lg-5 col-sm-12">
-          <div class="text-center">
-              <h2>WEBINAR</h2>
-          </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-            Instructor: PERSON 1<br/>
-            Description: <p> LOREM IPSUM </p>
-        </div>
+        <section class="row">
+              <div class="well col-lg-5 col-sm-12">
+              <div class="text-center">
+                  <h2>Security Webinar I</h2>
+              </div>
+              Title: Checking the Security of YOUR Servers<br/>
+              Date: December 21, 2016<br/>
+                Instructor: Ryan Sidebottom<br/>
+                Description: <p> In this workshop I will give you an introduction to securing your servers by showing you how to examine how secure they actually are. Securing a server is relatively simple once you know what is wrong, or insecure about it. Once you know what is insecure, you can work at making that insecurity more secure. Sometimes it is not possible to close off as many ports as possible but it is always possible to manage connections. We will go over some tools you can use to evaluate your server and then go over some remediation methods. <strong>Disclaimer: some or all of the tools we will discuss in this workshop can be used in a malicious manner. Never do anything you do not have permission to do. In the workshop we will be scanning our own server, so it is best that you also use your own server. You cannot use the tools against our server or any other server in which you do not have written permission to evaluate.</strong> </p>
+            </div>
+            <div class= "col-lg-2"></div><!--Spacer -->
+            <div class="well col-lg-5 col-sm-12">
+              <div class="text-center">
+                  <h2>Security Webinar II</h2>
+              </div>
+              Title: Bug Bounty and Ethical Hacking<br/>
+              Date: January 21, 2017<br/>
+                Instructor: Ryan Sidebottom<br/>
+                Description: <p> In this workshop we will go over what a Bug Bounty is and what Ethical Hacking is. Using some of the tools and techniques we teach you in the previous workshop, you can get permission from certain website and companies to try and “break” their website. Basically, you can do some Ethical Hacking and get paid. Most of the companies that offer a Bug Bounty program will be pretty secure so if you are truly interested in being successful and finding a weak link in a website, you will need to learn and research more than what I go over in this presentation. We will also do a brief recap of some of the topics covered in the last workshop since they are relevant in this one. <strong>Disclaimer: Some or all of the tools we will discuss in this workshop can be used in a malicious manner. We will be demonstrating on one of our personal servers again which are off limits to you. Remember, you cannot use the tools against our server or any other server in which you do not have written permission to attack or scan.</strong> </p>
+            </div>
+        </section>
 
-        <div class="well col-lg-5 col-sm-12">
-          <div class="text-center">
-              <h2>WEBINAR</h2>
-          </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-            Instructor: PERSON 1<br/>
-            Description: <p> LOREM IPSUM </p>
-        </div>
-        <div class= "col-lg-2"></div><!--Spacer -->
-        <div class="well col-lg-5 col-sm-12">
-          <div class="text-center">
-              <h2>WEBINAR</h2>
-          </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-            Instructor: PERSON 1<br/>
-            Description: <p> LOREM IPSUM </p>
-        </div>
+        <section class="row">
+              <div class="well col-lg-5 col-sm-12">
+              <div class="text-center">
+                  <h2>HTML/CSS Webinar I</h2>
+              </div>
+              Title: CSS3 Animations<br/>
+              Date: December 28, 2016<br/>
+                Instructor: Jacob Frank<br/>
+                Description: <p> In this workshop we’ll discuss the new changes and additions to css with the update to CSS3. In this, we’ll discuss css animations and transitions that websites are using today. We’ll also talk about SVG animations, controlling css keyframes, animation events, and what we can use these animations for. </p>
+            </div>
+            <div class= "col-lg-2"></div><!--Spacer -->
+            <div class="well col-lg-5 col-sm-12">
+              <div class="text-center">
+                  <h2>HTML/CSS Webinar II</h2>
+              </div>
+              Title: Using a CSS and HTML framework<br/>
+              Date: January 22, 2017<br/>
+                Instructor: Jacob Frank<br/>
+                Description: <p> For this workshop we’ll go over an intro to HTML/CSS frameworks. The main one we’ll be discussing is bootstrap, which is the most widely used one. Frameworks are used to make developing a web page faster. They create a grid for the developer and provide some sample css for things like buttons and links. The for this workshop is to bring you from a novice developer to a junior level one. Frameworks are most of the time created by companies but everyone has to start somewhere and with this we hope to give you an intro to one of the most widely used web development tools. </p>
+            </div>
+        </section>
 
-        <div class="well col-lg-5 col-sm-12 ">
-          <div class="text-center">
-              <h2>WEBINAR</h2>
-          </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-            Instructor: PERSON 1<br/>
-            Description: <p> LOREM IPSUM </p>
-        </div>
-        <div class= "col-lg-2"></div><!--Spacer -->
-        <div class="well col-lg-5 col-sm-12">
-          <div class="text-center">
-              <h2>WEBINAR</h2>
-          </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-            Instructor: PERSON 1<br/>
-            Description: <p> LOREM IPSUM </p>
-        </div>
-      </div>
+        <section class="row">
+              <div class="well col-lg-5 col-sm-12">
+              <div class="text-center">
+                  <h2>Java Webinar I</h2>
+              </div>
+              Title: Preparing for Java 9<br/>
+              Date: December 22, 2016<br/>
+                Instructor: Andrew Diana<br/>
+                Description: <p> Are you ready for all of the big changes in Java 9? I sure am. I’ve worked hard over the last few months to create an interactive workshop where you will be able to demo the new features that are not currently live. I will show you everything from minor changes that you won’t notice, to changes that will change the way that you code. Come check it out! Hope to see you there. </p>
+            </div>
+            <div class= "col-lg-2"></div><!--Spacer -->
+            <div class="well col-lg-5 col-sm-12">
+              <div class="text-center">
+                  <h2>Java Webinar II</h2>
+              </div>
+              Title: GUI Design<br/>
+              Date: January 30, 2017<br/>
+                Instructor: Andrew Diana<br/>
+                Description: <p> Hello World! Are you ready to create your SECOND GUI? In this webinar we will create a calculator in Java! This is a two part webinar where you will learn how to do the computations behind the scenes and then also how to display the information in a neat manner. </p>
+            </div>
+        </section>
+
+        <section class="row">
+              <div class="well col-lg-5 col-sm-12 ">
+              <div class="text-center">
+                  <h2>Database Webinar I</h2>
+              </div>
+              Title: Using java to connect to a database<br/>
+              Date: December 20, 2016<br/>
+                Instructor: Jesus Esquivel<br/>
+                Description: <p> In this workshop we will look into connecting our MySQL database using java. You can connect to a database in a number of ways using other languages including  Pyhton, and C# but we will look into java. From there we will be able to run queries from a main method in java. </p>
+            </div>
+            <div class= "col-lg-2"></div><!--Spacer -->
+            <div class="well col-lg-5 col-sm-12">
+              <div class="text-center">
+                  <h2>Database Webinar II</h2>
+              </div>
+              Title: 3rd and 4th normal forms<br/>
+              Date: January 20, 2017<br/>
+                Instructor: Jesus Esquivel<br/>
+                Description: <p> We will now get into specifics in how to properly organize our databases. In the past we’ve talked about 1st and 2nd forms but things start to get a little tricky and more strict in the following 2. </p>
+            </div>
+        </section>
     </div>
 
     <?php include 'footer.php' ?>

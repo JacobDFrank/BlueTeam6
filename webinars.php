c910aad76447eccc15fb74ec81eede5cabefa2bf
Ryan Sidebottom <daboss@DaBoss.hsd1.nj.comcast.net>
2016-11-26 01:20:21 -0500

    Created more descriptive titles, added security webinars

--- webinars.php
+++ webinars.php
@@ -13,7 +13,7 @@
 
         <div class="well col-lg-5 col-sm-12">
           <div class="text-center">
-            <h2>WEBINAR</h2>
+            <h2>Android Webinar I</h2>
           </div>
           Title: Vector Drawables<br/>
           Date: December 29, 2016<br/>
@@ -23,7 +23,7 @@
         <div class= "col-lg-2"></div><!--Spacer -->
         <div class="well col-lg-5 col-sm-12"> 
           <div class="text-center">
-              <h2>WEBINAR</h2>
+              <h2>Android Webinar II</h2>
           </div>
           Title: Material Design<br/>
           Date: January 17, 2017<br/>
@@ -32,22 +32,22 @@
         </div>
         <div class="well col-lg-5 col-sm-12">
           <div class="text-center">
-              <h2>WEBINAR</h2>
+              <h2>Security Webinar I</h2>
           </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-            Instructor: PERSON 1<br/>
-            Description: <p> LOREM IPSUM </p>
+          Title: Checking the Security of YOUR Servers<br/>
+          Date: December 21, 2016<br/>
+            Instructor: Ryan Sidebottom<br/>
+            Description: <p> In this workshop I will give you an introduction to securing your servers by showing you how to examine how secure they actually are. Securing a server is relatively simple once you know what is wrong, or insecure about it. Once you know what is insecure, you can work at making that insecurity more secure. Sometimes it is not possible to close off as many ports as possible but it is always possible to manage connections. We will go over some tools you can use to evaluate your server and then go over some remediation methods. <strong>Disclaimer: some or all of the tools we will discuss in this workshop can be used in a malicious manner. Never do anything you do not have permission to do. In the workshop we will be scanning our own server, so it is best that you also use your own server. You cannot use the tools against our server or any other server in which you do not have written permission to evaluate.</strong> </p>
         </div>
         <div class= "col-lg-2"></div><!--Spacer --> 
         <div class="well col-lg-5 col-sm-12">
           <div class="text-center">
-              <h2>WEBINAR</h2>
+              <h2>Security Webinar II</h2>
           </div>
-          Title: INSERT TITLE HERE<br/>
-          Date: DATE HERE<br/>
-            Instructor: PERSON 1<br/>
-            Description: <p> LOREM IPSUM </p>
+          Title: Bug Bounty and Ethical Hacking<br/>
+          Date: January 21, 2017<br/>
+            Instructor: Ryan Sidebottom<br/>
+            Description: <p> In this workshop we will go over what a Bug Bounty is and what Ethical Hacking is. Using some of the tools and techniques we teach you in the previous workshop, you can get permission from certain website and companies to try and “break” their website. Basically, you can do some Ethical Hacking and get paid. Most of the companies that offer a Bug Bounty program will be pretty secure so if you are truly interested in being successful and finding a weak link in a website, you will need to learn and research more than what I go over in this presentation. We will also do a brief recap of some of the topics covered in the last workshop since they are relevant in this one. <strong>Disclaimer: Some or all of the tools we will discuss in this workshop can be used in a malicious manner. We will be demonstrating on one of our personal servers again which are off limits to you. Remember, you cannot use the tools against our server or any other server in which you do not have written permission to attack or scan.</strong> </p>
         </div>
 
         <div class="well col-lg-5 col-sm-12">

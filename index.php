4acaf8c0d037994892e33accaed400b89f6ef7a4
Ben Alderfer <benalderfer974@gmail.com>
2016-12-01 22:59:34 -0500

    about us editing

--- index.php
+++ index.php
@@ -27,13 +27,13 @@
         <div class="homepage_column col-md-8 well">
             <h3>About us</h3>
             <h4 class="homepage_section-heading">Who We Are:</h4>
-            <p class="homepage_section-paragraph">We are a group of students from the Rochester Institute of Technology. Ranging from Sophomores to Juniors, among us- we have over 10 years of technical expertise. Using knowledge from school as well as knowledge from our own personal experiences, we can solve nearly any technical issue that you may have.</p>
+            <p class="homepage_section-paragraph">We are a group of students from the Rochester Institute of Technology. Ranging from Sophomores to Juniors, among us - we have over 10 years of technical expertise. Using knowledge from school as well as knowledge from our own personal experiences, we can solve nearly any technical issue that you may have.</p>
 
             <h4 class="homepage_section-heading">Are you ready to learn?</h4>
-            <p class="homepage_section-paragraph">Are you ready to gain some hands on experience? You have come to the right place. Our professionals have developed in depth tutorials- designed for a common user, we are confident that you will leave with an aura of confidence in the field(s) that you have chosen.We currently have tutorials in mySQL, Java, Android dev, HTML/CSS, and computer security. Select one and further your education today.</p>
+            <p class="homepage_section-paragraph">Are you ready to gain some hands on experience? You have come to the right place. Our professionals have developed in depth tutorials - designed for a common user, we are confident that you will leave with an aura of confidence in the field(s) that you have chosen. We currently have tutorials in mySQL, Java, Android dev, HTML/CSS, and computer security. Select one and further your education today.</p>
 
             <h4 class="homepage_section-heading">What you need?</h4>
-            <p class="homepage_section-paragraph">For many of these tutorials- you may need to download a program or two to run the code and follow along. Our staff has done extensive research on the software that we recommend to you and assure that it is safe to install as well as simple to use.’</p>
+            <p class="homepage_section-paragraph">For many of these tutorials - you may need to download a program or two to run the code and follow along. Our staff has done extensive research on the software that we recommend to you and assure that it is safe to install as well as simple to use.</p>
 
             <h4 class="homepage_section-heading">WE want YOU to learn.</h4>
             <p class="homepage_section-paragraph">We believe that anyone can learn to program or how to system test. That is why we are 100% confident that by the end of our tutorials, you will be highly knowledgeable in that given field.</p>

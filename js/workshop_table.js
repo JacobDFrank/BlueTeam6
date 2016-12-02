c6deef9ad70c7d69290447b42cdcbf9d76c89347
Ryan Sidebottom <daboss@daboss.wireless.rit.edu>
2016-12-01 22:35:35 -0500

    Fixed workshop reload after DB change

--- js/workshop_table.js
+++ js/workshop_table.js
@@ -7,8 +7,12 @@ function addPerson(className) {
         url: '/~blueteam/project/php/workshop-process.php',
         data: { type: "add", name: workshopName },
         success:function() {
-         window.location.reload();
-      }
+            window.location.reload();
+        },
+        error:function() {
+            window.location.reload();
+            alert("Failed to add you to list.");
+        }
     });
 }
 
@@ -19,6 +23,13 @@ function removePerson(className) {
     $.ajax({
         type: "POST",
         url: "php/workshop-process.php",
-        data: { type: "remove", name: workshopName }
+        data: { type: "remove", name: workshopName },
+        success:function() {
+            window.location.reload();
+        },
+        error:function() {
+            window.location.reload();
+            alert("Failed to remove you from list.");
+        }
     });
 }

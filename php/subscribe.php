0f21933647efc05ac06f696c5f2bb00183dea45c
Ben Alderfer <benalderfer974@gmail.com>
2016-12-03 00:51:35 -0500

    subscribe working and styled

--- php/subscribe.php
+++ php/subscribe.php
@@ -1,19 +1,40 @@
-<?php
-$email = $_POST["email"];
-$servername ="localhost";
-$username = "blueteam";
-$password = "fr1end";
-$dbname = "blueteam";
-$table = "subscribers";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Subscribed</title>
+    <?php include '../inner-head.php' ?>
+</head>
+<body>
+    <?php include '../inner-header.php' ?>
 
-//Connect to mySQL server
-$conn = new mysqli($servername, $username, $password, $dbname);
+    <?php
+    $email = $_POST["email"];
+    $servername ="localhost";
+    $username = "blueteam";
+    $password = "fr1end";
+    $dbname = "blueteam";
+    $table = "subscribers";
 
-//If connection failed, die.
-if($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    //Connect to mySQL server
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-$sqlInsert = "INSERT INTO subscribers (subscriberID, subscriberEmail) VALUES(NULL, '".$email."');";
-$result = $conn->query($sqlInsert);
-?>
+    //If connection failed, die.
+    if($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sqlInsert = "INSERT INTO subscribers (subscriberID, subscriberEmail) VALUES(NULL, '".$email."');";
+    $result = $conn->query($sqlInsert);
+    ?>
+
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 well">
+            <h4>You are now subscribed. Check your email for a confirmation and you will receive the next newsletter we send out.</h4>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+
+    <?php include '../inner-footer.php' ?>
+</body>
+</html>

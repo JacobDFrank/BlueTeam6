ed1aa9a55f2242acf0a56a7d3c9d8dfc5989828c
axd7832 <axd7832@g.rit.edu>
2016-12-04 17:22:47 -0500

    Grading

--- grading.php
+++ grading.php
@@ -8,7 +8,13 @@
 	<?php include 'header.php' ?>
 		<h1>Grading</h1>
 		<section class="container">
-		
+		<h2> What we did well: </h2>
+			<ul>
+				<li>Our quiz uses php to figure out how many were correct, and then displays the information back to the user</li>
+				<li>Our Webinar page originally had containers that were all different sizes. By changing the display of the rows to flew: the problem was solved</li>
+				<li>We tried to keep our color scheme as consistant as possible across all of our pages.</li>
+				<li></li>
+			</ul>
 		</section>
 	<?php include 'footer.php' ?>	
 	</body>

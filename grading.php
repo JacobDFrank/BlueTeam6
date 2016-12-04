29947000f4fdc903a1bcfa310647a35c6ba52aa5
Ben Alderfer <benalderfer974@gmail.com>
2016-12-04 18:27:41 -0500

    added to grading

--- grading.php
+++ grading.php
@@ -10,10 +10,14 @@
 		<section class="container">
 		<h2> What we did well: </h2>
 			<ul>
+                <li>Our newsletter contains upcoming events and featured sections from blogs, workshops, and webinars.
+                    It also has a working subscribe form that adds their email to a table.</li>
 				<li>Our quiz uses php to figure out how many were correct, and then displays the information back to the user</li>
 				<li>Our Webinar page originally had containers that were all different sizes. By changing the display of the rows to flew: the problem was solved</li>
-				<li>We tried to keep our color scheme as consistant as possible across all of our pages.</li>
-				<li></li>
+                <li>Our tutorials have a variety of step layout options and special formatting for code.</li>
+                <li></li>
+                <li>We tried to keep our color scheme as consistent as possible across all of our pages.</li>
+				<li>Our pages are based on templates so they look uniform.</li>
 			</ul>
 		</section>
 	<?php include 'footer.php' ?>	

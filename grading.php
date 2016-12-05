93edf67b6d9737f178a69ffc8ab98855110dc937
Ben Alderfer <benalderfer974@gmail.com>
2016-12-04 21:33:10 -0500

    censored

--- grading.php
+++ grading.php
@@ -15,15 +15,11 @@
 				<li>Our quiz uses php to figure out how many were correct, and then displays the information back to the user</li>
 				<li>Our Webinar page originally had containers that were all different sizes. By changing the display of the rows to flew: the problem was solved</li>
                 <li>Our tutorials have a variety of step layout options and special formatting for code.</li>
-                <li></li>
                 <li>We tried to keep our color scheme as consistent as possible across all of our pages.</li>
 				<li>Our pages are based on templates so they look uniform.</li>
 				<li>We have a list of FAQ that users may ask themselves relating to the content of each of our profiles.</li>
 				<li>Added a sitemap of the entire page that users can see and navigate from there</li>
-				<li>We included this grading to layout evertything we have accomplished thus far in our project</li>
                 <li>As a team, we set up goals for each week almost every single time, we hit them.</li>
-				<li>Blue Team 6 had no trouble cooperating and talking, everything that needed to be done was discussed in a collaborative manor.</li>
-				<li>Most everyone put in their fair share of work and those that needed help on something they couldn't figure out got it.</li>
 			</ul>
 		</section>
 	<?php include 'footer.php' ?>
